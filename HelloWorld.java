4714034841638915fee059573b6376a86c46d950
Daniel Bolias <dbolias@cpp.edu>
2025-02-04 08:10:59 -0800

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+// Daniel Bolias
+// CS 1400
+// Assigment 1
+// September 4, 2024
+
+class HelloWorld
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hello World, my name is Daniel Bolias, and I like soccer. I have been programming for 2 years");
+    }
+}
